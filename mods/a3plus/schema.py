d41072c877635b169f56e723d0adbce5436da0a7
AyahBarudak <ayah.syahid2017@gmail.com>
2026-07-12 21:23:29 +0700

    Add: Tabel part

--- mods/a3plus/schema.py
+++ mods/a3plus/schema.py
@@ -8,6 +8,19 @@ CREATE TABLE IF NOT EXISTS toner_counter (
     counter_type TEXT NOT NULL CHECK (counter_type IN ('BW', 'FULLCOLOR')),
     counter_value INTEGER NOT NULL
 );'''
-_schema_v1 = [_toner_counter_v1]
 
-schemas = [_schema_v1]
+_part_replacement_log_v1 = '''
+CREATE TABLE IF NOT EXISTS a3_part_replacement (
+    id INTEGER PRIMARY KEY,
+    part_name TEXT NOT NULL COLLATE NOCASE,
+    install_date TEXT NOT NULL,
+    installer TEXT NOT NULL,
+    counter_fullcolor_value INT NOT NULL,
+    description TEXT NOT NULL DEFAULT '{}' CHECK ( json_valid(description) = 1)
+);
+'''
+
+_schema_v1 = [_toner_counter_v1, _part_replacement_log_v1]
+_schema_v2 = [_part_replacement_log_v1]
+
+schemas = [_schema_v1, _schema_v2]
